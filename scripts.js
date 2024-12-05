5646276caa36c89b0a4d25794ca6e820502477da
joseph24augusto27 <josephinho2000@gmail.com>
2024-12-04 23:25:19 -0300

    projeto mario atualizado(dev-club)

--- scripts.js
+++ scripts.js
@@ -1,6 +1,20 @@
-const form = document.querySelector("formulario-fale-conosco")
+const form = document.querySelector(".formulario-fale-conosco")
+const mascara = document.querySelector(".mascara-formulario")
 
 
-function cliqueiNoBotao(){
- console.log(form)
+
+
+
+function mostrarForm () {
+    form.style.left = "50%"
+    form.style.transform = "translateX(-50%)"
+    mascara.style.visibility = "visible"
 }
+
+function esconderForm(){
+    form.style.left="-300%"
+    form.style.transform = "translateX(0)"
+    mascara.style.visibility = "hidden"
+}
+
+
